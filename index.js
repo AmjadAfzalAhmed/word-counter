2a428588d7a210dd92deeb247b7e928c5758c170
Amjad <abhutto2006@gmail.com>
2024-04-18 22:54:39 +0500

    task completed successfully

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+async function wordCount() {
+    let answers = await inquirer.prompt([
+        {
+            name: "Sentence",
+            type: "input",
+            message: chalk.greenBright.bold("Enter your sentence to count the words in it:")
+        },
+        {
+            name: "Sentence1",
+            type: "input",
+            message: chalk.yellowBright.bold("Enter your sentence to chound the letters in it:")
+        }
+    ]);
+    let words = answers.Sentence.trim().split(" ");
+    console.log("\n", words);
+    console.log(chalk.blueBright.bold(`\n Your sentence contains a total number of ${words.length} words.`));
+    let letters = answers.Sentence1.replace(/\s/g, "").length;
+    console.log(chalk.greenBright.bold(`\n Your sentence consists of total ${letters} letters.`));
+}
+wordCount();
